ece8636eb922942a2dd56d8003de6e8afe0404d2
r25347sh <r25347sh@hs.reitaku.jp>
2026-09-03 16:48:12 +0900

    G5 Portal: users.json, shift.json, sw, manifest

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
-/* G⁵ Portal Service Worker */
 const CACHE = "g5-portal-v2";
-const ASSETS = ["./","./index.html","./shift.html","./manual.html","./admin.html","./css/style.css","./css/shift.css","./css/manual.css","./css/admin.css","./css/index.css","./js/common.js","./js/load.js","./js/notif.js","./menu/menu.css","./menu/menu.js","./manifest.json","./icons/icon-192.png","./icons/icon-512.png"];
+const ASSETS = ["./","./index.html","./shift.html","./manual.html","./admin.html","./css/style.css","./js/common.js","./js/load.js","./manifest.json"];
 self.addEventListener("install", (e) => { e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS).catch(() => {})).then(() => self.skipWaiting())); });
 self.addEventListener("activate", (e) => { e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener("fetch", (e) => { const url = new URL(e.request.url); if (url.pathname.includes("/src/data/")) { e.respondWith(fetch(e.request).catch(() => caches.match(e.request))); return; } e.respondWith(caches.match(e.request).then((cached) => { const fetched = fetch(e.request).then((res) => { if (res && res.ok && e.request.method === "GET") { const clone = res.clone(); caches.open(CACHE).then((c) => c.put(e.request, clone)); } return res; }).catch(() => cached); return cached || fetched; })); });
