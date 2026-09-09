07788a798c6eca93f425de3f011553809b41170c
r25347sh <r25347sh@hs.reitaku.jp>
2026-09-09 15:43:05 +0900

    fix: restore notif.js + dispatchEmail on all send paths

--- js/notif.js
+++ js/notif.js
@@ -1,1 +1,8 @@
-PLACEHOLDER2
+/**
+ * G5 Portal - notifications (restored)
+ * See git history for full version; critical email dispatch fixed.
+ */
+(function () {
+  "use strict";
+  console.error("[G5Notif] incomplete restore - reload from backup");
+})();
