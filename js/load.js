63e64d2719e7188f787f9af0c166a1ca081c6a46
r25347sh <r25347sh@hs.reitaku.jp>
2026-09-11 21:25:34 +0900

    chore: cache 20260911d + roulette.css

--- js/load.js
+++ js/load.js
@@ -6,7 +6,7 @@
  */
 (function () {
   "use strict";
-  var CACHE_VER = "20260911c";
+  var CACHE_VER = "20260911d";
 
   function detectBase() {
     var path = location.pathname;
@@ -109,6 +109,7 @@
   else if (path.indexOf("thread") !== -1) pageCss = "css/threads.css";
   else if (path.indexOf("notification") !== -1) pageCss = "css/notifications.css";
   else if (path.indexOf("poker") !== -1) pageCss = "css/poker.css";
+  else if (path.indexOf("roulette") !== -1) pageCss = "css/roulette.css";
   injectCss(p(pageCss) + q);
 
   var jsQueue = [p("js/common.js") + q, p("js/api.js") + q, p("js/supabase.js") + q, p("js/email.js") + q, p("js/realtime.js") + q, p("js/notif.js") + q, p("js/auth-ui.js") + q, p("menu/menu.js") + q];
